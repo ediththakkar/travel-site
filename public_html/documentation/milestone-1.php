8277ec3704521a1b5413a6427b24998db451ffc2
Edith Thakkar <egarcia262@cnm.edu>
2018-04-24 22:12:52 -0600

    alignment, css addition, and styles

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,28 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 	<head>
+	<style>
+	h1 {
+	background-color: khaki;
+	}
+	p {
+	font-size: 25px;
+	font-family: "Arial Hebrew";
+	font-style: italic;
+	}
+		</style>
 	<meta charset="UTF-8">
+	<link href="styles.css" rel="stylesheet" type="text/css">
 	<title>Milestone 1</title>
-<body>
-		<h1>Purpose, Audience, Goal</h1>
-		<ol>
-			<li><strong>Purpose:</strong>&nbsp;I would like my PWP to help me launch my travel and tourism company</li>
-			<li><strong>Audience:</strong>&nbsp;This site would be viewed from people across the world who wish to travel. It can be professionals, families, solo travelers, or groups. Anyone with a passion to travel.</li>
-			<li><strong>Goal:</strong>&nbsp;To increase exposure for travel outside of the USA for individuals.</li>
-		</ol>
-		<img src="cosby-4.jpg">
-		<h1>Persona</h1>
-		<ul>
-			<li><strong>Name:</strong> Joan Stallion</li>
-			<li><strong>Demographic Information:</strong> Washington, Virginia</li>
-			<li><strong>Age:</strong> 35</li>
-			<li><strong>Job:</strong> Nonprofit Executive Director</li>
-			<li><strong>Relevant technology:</strong> Macbook Pro, Iphone 10 Plus</li>
-			<li><strong>Behaviors and Attitudes:</strong> Joan loves public speaking, advocating for the community, and loves traveling the world. She enjoys meditation, and dance, and lives life day by day.</li>
-			<li><strong>Scenario:</strong>Joan is in search of a site she can use to easily book her next vacation with ease. </li>
-			<li><strong>Goals:</strong> Easily navigate throughout the site. Have ease of creating an account.</li>
-		</ul>
-	</head>
+	<body>
+	<h1>Purpose, Audience, Goal</h1>
+	<body> <p align="left">
+	<li><strong>Purpose:</strong>&nbsp;I would like my PWP to help me launch my travel and tourism company by spreading the word of beautiful places to visit</li>
+	<li><strong>Audience:</strong>&nbsp;This site would be viewed from people across the world who wish to travel. It can be professionals, families, solo travelers, or groups. Anyone with a passion to travel.</li>
+	<li><strong>Goal:</strong>&nbsp;To increase exposure for travel outside of the USA for individuals.</li></p>
+	<h1>Persona</h1>
 	</body>
+	<img src="P1010011Thailand_Bloggers_.jpg">
+	</body>
+	<body><<p align="left"
+		<p><li><strong>Name:</strong> Joan Stallion</li></p>
+  		<p><li><strong>Demographic Information:</strong> Washington, Virginia</li></p>
+		<p><li><strong>Age:</strong> 35</li></p>
+		<p><li><strong>Job:</strong> Nonprofit Executive Director</li></p>
+		<p><li><strong>Relevant technology:</strong> Macbook Pro, Iphone 10 Plus</li></p>
+		<p><li><strong>Behaviors and Attitudes:</strong> Joan loves public speaking, advocating for the community, and loves traveling the world. She enjoys meditation, and dance, and lives life day by day.</li></p>
+		<p><li><strong>Scenario:</strong>Joan is in search of a site where she can find ideas of places to visit on her next vacation with ease.</li></p>
+		<p><li><strong>Goals: Easily navigate throughout the site. Have ease of contacting company for more information.</li></p>
+	</body></p>
+	</head>
 
 </html>
