71720bd0ccd422bb290b7fc787cccee2557d3900
Edith Thakkar <egarcia262@cnm.edu>
2018-05-02 22:41:25 -0600

    created mobile style, and full screen style wireframes

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -12,27 +12,27 @@
 	}
 		</style>
 	<meta charset="UTF-8">
-	<link href="styles.css" rel="stylesheet" type="text/css">
+	<link href="../../style.css" rel="stylesheet" type="text/css">
 	<title>Milestone 1</title>
 	<body>
 	<h1>Purpose, Audience, Goal</h1>
 	<body> <p align="left">
-	<li><strong>Purpose:</strong>&nbsp;I would like my PWP to help me launch my travel and tourism company by spreading the word of beautiful places to visit</li>
-	<li><strong>Audience:</strong>&nbsp;This site would be viewed from people across the world who wish to travel. It can be professionals, families, solo travelers, or groups. Anyone with a passion to travel.</li>
-	<li><strong>Goal:</strong>&nbsp;To increase exposure for travel outside of the USA for individuals.</li></p>
+	<li><strong>Purpose:</strong>&nbsp;The purpose of this site is to create a informational site for travelers who are seeking recommendations to new places to travel to.</li>
+	<li><strong>Audience:</strong>&nbsp;The audience for this site is for individuals of 18+ who are looking to travel across the world. It's directed towards families, solo travelers, or groups. Anyone with a passion to travel.</li>
+	<li><strong>Goal:</strong>&nbsp;To create a successful functional user-friendly site for individuals to look for informational information on travel suggestions.</li></p>
 	<h1>Persona</h1>
 	</body>
 	<img src="P1010011Thailand_Bloggers_.jpg">
 	</body>
-	<body><<p align="left"
-		<p><li><strong>Name:</strong> Joan Stallion</li></p>
-  		<p><li><strong>Demographic Information:</strong> Washington, Virginia</li></p>
+	<body><p align="left"
+		<p><strong>Name:</strong> Joan Stallion</p>
+  		<p><strong>Demographic Information:</strong> Washington, Virginia</p>
 		<p><li><strong>Age:</strong> 35</li></p>
 		<p><li><strong>Job:</strong> Nonprofit Executive Director</li></p>
 		<p><li><strong>Relevant technology:</strong> Macbook Pro, Iphone 10 Plus</li></p>
 		<p><li><strong>Behaviors and Attitudes:</strong> Joan loves public speaking, advocating for the community, and loves traveling the world. She enjoys meditation, and dance, and lives life day by day.</li></p>
-		<p><li><strong>Scenario:</strong>Joan is in search of a site where she can find ideas of places to visit on her next vacation with ease.</li></p>
-		<p><li><strong>Goals: Easily navigate throughout the site. Have ease of contacting company for more information.</li></p>
+		<p><li><strong>Scenario:</strong>Joan is in search of a site where she can find information/recommendations of places to visit on her next vacation with ease.</li></p>
+		<p><li><strong>Goals: Easily navigate throughout the site. Have ease of contacting company for more information about travel suggestions.</li></p>
 	</body></p>
 	</head>
 
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<title>Content Strategy</title>
+	</head>
+	<body>
+	<h1>Content Strategy</h1>
+	<p>
+	<h2>Homepage</h2>
+	The homepage of my site will feature different sections. It will be a one page only. Below are the following sections it will contain.
+	<p>
+	<strong>Example:</strong> Welcome to Travel track. Travel track is an informational travel site where you can find ideas for your next out of country vacation. Where your able to not only find suggestions, but are able to get connected with tour guides, and host families.
+	</p>
+	<p>
+	<h2>About Us</h2>
+	The about us section will feature an introductory on how Travel track was created, its founders, informational travel destinations, and recommendations.
+	<p>
+	<strong>Example:</strong>Travel Track was created with an idea of coming up with ways for travelers to get recommendations to fuel their travel desires. Not only are they recommended places to travel to but are also given resources to help them manage their travels more easily.
+	</p>
+	<p>
+	<h2>Travel</h2>
+	</p>
+	<p>The Travel section will feature different countries with recommendational information, images, links to resources, and videos. Along with an interactional map.</p>
+	<strong>Example: Where would you like to travel to? To Asia, South America, Africa, or maybe Europe?</strong>
+	<p>
+	<h2>Contact Us</h2>
+		<p>The Contact us section features a contact form with message option, along with  a like and subscribe button.</p>
+	<strong>Example: Contact us section is great feature for individuals interested in learning more can contact the site. </strong>
+	<body>
+		<h2>Wireframes</h2>
+		<li><strong>Mobile Layout</strong></li>
+		<img src="Mobilesite.png">
+	</body>
+		<body>
+			<li><strong>Full Screen Layout</strong></li>
+		<img src="Website.png">
+		</body>
+	</body>
+</html>
+
