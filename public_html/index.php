d5b5cb74e7c5f0b6ad3eb8b9413d113c4e3792e0
Edith Thakkar <egarcia262@cnm.edu>
2018-05-20 18:38:10 -0600

    added css, bootstrap, nav bar

---
+++ public_html/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Edith Thakkar PWP</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="css/style.css">
+		</div>
+
+</header>
+
+		<!--BOOTSTRAP CSS-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<!--jQuery first, then popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+		<!-- JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+	</head>
+	<body>
+
+		<!--Navigation style-->
+<style>
+	body {
+		margin: 0;
+		padding:0;
+	}
+
+	nav{
+		width: 100%;
+		background: darkkhaki;
+		overflow: visible;
+	}
+
+	ul{
+		padding:0;
+		margin:0 0 0 150px;
+		list-style: none;
+	}
+
+	li{
+		float: left;
+	}
+
+	nav a{
+		width:250px;
+		display: flex;
+		padding:20px 15px;
+		text-decoration: aqua;
+		font-style: normal;
+		color: white;
+		text-transform: uppercase;
+		text-align: right;
+	}
+
+	nav a:hover{
+		background: gold;
+		transition: 0.5s;
+		text-transform: uppercase ;
+		text-align: right;
+	}
+
+</style>
+		<!--Navigation bar-->
+		<body data-spy="scroll" data-target='.navbar' data-offset='50'>
+			<nav class="navbar navbar-expand-sm bg dark navbar-dark sticky-top">
+				<ul class="navbar-nav">
+					<li class="nav-item"><a href="#HOME" class="nav-link active">HOME</a></li>
+					<li class="nav-item"><a href="#ABOUT US" class="nav-link active">ABOUT US</a></li>
+					<li class="nav-item"><a href="#TRAVEL" class="nav-link active">TRAVEL</a></li>
+					<li class="nav-item"><a href="#CONTACT US" class="nav-link active">CONTACT US</a></li>
+					</div>
+				</ul>
+			</nav>
+
+		<!--Logo-->
+			<div class="logo"><a href="#"><img src="images/LogoMakr_7iiiAg.png"></a>
+			</div>
+		<!--Home section-->
+
+	<div id="HOME" class="container-fluid" style="padding-top:70px;padding-bottom:70px">
+		<h1>HOME</h1>
+		<p>Welcome to our site Travel Track. </p>
+	</div>
+
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+
+			<!--About Us section-->
+	<div id="ABOUT US" class="container-fluid" style="padding-top:70px;padding-bottom:70px">
+		<h1>ABOUT US</h1>
+		<p>r3r3r3r3r23r32r4fdsfrgrgwef</p>
+	</div>
+
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+
+			<!--Travel section-->
+	<div id="TRAVEL" class="container-fluid" style="padding-top:70px;padding-bottom:70px">
+		<h1>TRAVEL</h1>
+		<p>r3r3r3r3r23r32r4fdsfrgrgwef</p>
+	</div>
+
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+
+			<!--Contact Us section-->
+	<div id="CONTACT US" class="container-fluid" style="padding-top:70px;padding-bottom:70px">
+		<h1>Contact Us</h1>
+		<p>r3r3r3r3r23r32r4fdsfrgrgwef</p>
+	</div>
+</body>
+
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+
+	</body>
