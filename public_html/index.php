ce9f1f704653b8ddea9e94bb4f2cc09738ef53fd
Edith Thakkar <egarcia262@cnm.edu>
2018-05-24 16:47:12 -0600

    contact us section header, div class, and beginning of inputs

--- public_html/index.php
+++ public_html/index.php
@@ -1,92 +1,53 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
 	<head>
 		<meta charset="UTF-8" />
 		<title>Edith Thakkar PWP</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" href="css/style.css">
-		</div>
-
-</header>
 
 		<!--BOOTSTRAP CSS-->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 		<!--jQuery first, then popper.js, then Bootstrap JS -->
+
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
+		<!--Fonts-->
+		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+		<!-- Linked CSS stylesheet-->
+		<link rel="stylesheet" href="css/style.css">
+
 		<!-- JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
 	</head>
-	<body>
-
-		<!--Navigation style-->
-<style>
-
-	ul{
-		padding:0;
-		margin:0 0 0 150px;
-		list-style: none;
-	}
-
-	li{
-		float: left;
-	}
-
-	nav a{
-		width:250px;
-		display: flex;
-		padding:20px 15px;
-		text-decoration: aqua;
-		font-style: normal;
-		color: white;
-		text-transform: uppercase;
-		text-align: right;
-	}
-
-	nav a:hover{
-		background: tan;
-		transition: 0.5s;
-		text-transform: uppercase ;
-		text-align: right;
-	}
-
-	nav{
-		width:100%;
-		background: darkkhaki;
-		overflow: visible;
-	}
-
-	input[type=text],input[type-email],textarea{display: flex; -webkit-box-sizing: border-box;
-	-moz-box-sizing: border-box;
-	box-sizing: border-box; width: 100%;}
-	div#glob_content{ background-color: goldenrod; max-width: 450}
-
-</style>
+<body>
+	<div>
 		<!--Navigation bar-->
-		<body
+			<nav class="navbar">
 			<div class style="text-align: center" data-spy="scroll" data-target='.navbar' data-offset='50'>
-			<nav class="navbar navbar-expand-sm bg dark navbar-dark sticky-top">
+				<nav class="navbar navbar-expand-sm bg dark navbar-dark sticky-top"></nav>
 				<ul class="navbar-nav">
 					<li class="nav-item"><a href="#HOME" class="nav-link active">HOME</a></li>
 					<li class="nav-item"><a href="#ABOUT US" class="nav-link active">ABOUT US</a></li>
 					<li class="nav-item"><a href="#TRAVEL" class="nav-link active">TRAVEL</a></li>
 					<li class="nav-item"><a href="#CONTACT US" class="nav-link active">CONTACT US</a></li>
-					</div>
 				</ul>
-			</nav>
+			</div>
+					</div>
+			</>
 
 		<!--Logo-->
 			<div class="logo"><a href="#"><img src="images/LogoMakr_7iiiAg.png"></a>
 			</div>
 		<!--Home section-->
 
-	<div id="HOME" class="container-fluid" style="padding-top:70px;padding-bottom:70px">
+	<div id="HOME" class="container-fluid" style="padding-top:70px;padding-bottom:70px"></div>
 		<h1>HOME</h1>
 		<p>Welcome to Travel track. Travel track is an informational travel site where you can find ideas for your next out of country vacation. Where your able to not only find suggestions, but are able to get connected with tour guides, and host families</p>
-	</div>
+
 
 			<br>
 			<br>
@@ -112,6 +73,7 @@
 	<div id="TRAVEL" class="container-fluid" style="padding-top:70px;padding-bottom:70px">
 		<h1>TRAVEL</h1>
 		<p>Where would you like to travel to? To Asia, South America, Africa, or maybe Europe?</p>
+		<p>Chupa chups tootsie roll marzipan. Cupcake chupa chups marshmallow. Tootsie roll tootsie roll dragée. Dessert ice cream donut cotton candy. Cupcake brownie marshmallow. Danish bonbon lemon drops. Dessert tart macaroon sweet roll chocolate bonbon icing pie. Pudding icing chocolate cake chocolate cake bear claw tootsie roll. Apple pie sesame snaps chupa chups jelly beans. Brownie macaroon jelly-o caramels brownie marzipan gingerbread ice cream. Danish tootsie roll chocolate cake jujubes marshmallow muffin. Bear claw muffin cupcake toffee marshmallow tart bonbon gummies biscuit.</p>
 	</div>
 
 			<br>
@@ -122,37 +84,13 @@
 			<br>
 
 			<!--Contact Us section-->
-	<div id="CONTACT US" class="container-fluid" style="padding-top:70px;padding-bottom:70px">
-		<h1>Contact Us</h1>
-		<p>Chocolate cake caramels croissant topping chocolate bar bonbon. Oat cake lollipop jelly beans marshmallow biscuit. Toffee brownie lollipop biscuit sweet roll biscuit. Tart gummies tart. Pudding icing toffee marshmallow jelly-o. Jelly beans donut halvah jelly beans chocolate toffee bear claw pie. Dragée cotton candy wafer candy biscuit pastry soufflé sugar plum cotton candy. Soufflé muffin apple pie. Pie pie dessert halvah candy tart cheesecake. Bear claw cupcake pie cheesecake cookie toffee ice cream apple pie. Fruitcake cake pie fruitcake jelly pudding liquorice caramels. Chocolate bar sweet roll biscuit bear claw sweet. Sweet ice cream macaroon candy canes tiramisu.</p>
-	</div>
-</body>
-<body>
 
-
-	<div id="glob-content">
-
-		<div id="title"></div>
-
-		<div id="form_content">
-
-			<form>
-				<input type="text" name="name" placeholder="Name" />
-				<input type="email" name="email" placeholder="Email" />
-				<input type="text" name="phone" placeholder="Phone" />
-				<textarea name="message" placeholder="Message" ></textarea>
-				<input type="submit" name="send" value="send" />
-
-			</form>
-		</div>
-
-	</div>
-
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-
-	</body>
+	<body>
+		<header>
+			<h1>Contact Us</h1>
+		</header>
+		<div class="contact">
+		<form id="contact" method="post" action="">
+			<h2>Contact Form</h2>
+		<label>Name :</label><br>
+		<input type="text" placeholder=""Enter u R name"
