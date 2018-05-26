83be0e189417608ecea29e6bed9801c589d0dfc7
Edith Thakkar <egarcia262@cnm.edu>
2018-05-26 13:22:57 -0600

    completion of navigation bar

--- public_html/index.php
+++ public_html/index.php
@@ -25,30 +25,30 @@
 	<body>
 
 <!--------------------- Navigation bar ----------------------------------->
-<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
-<div class="container-fluid">
-	<a class="nav-brand" href="#"><img src="images/LogoMakr_7iiiAg%20copy2.png"></a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse"
-	data-target="#navbarResponsive">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-<div class="collapse navbar-collapse" id="navbarResponsive">
-	<ul class="navbar-nav ml-auto">
-		<li class="nav-item active">
-			<a class="nav-link" href="#">Home</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">About Us</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Travel</a>
-		</li>
-	<li class="nav-item">
-		<a class="nav-link" href="#">Contact Us</a>
-	</li>
-</div>
-</div>
-</nav>
+		<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+			<div class="container-fluid">
+				<a class="nav-brand" href="#"><img src="images/LogoMakr_7iiiAg%20copy2.png"></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse"
+						  data-target="#navbarResponsive">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">About Us</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Travel</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Contact Us</a>
+						</li>
+				</div>
+			</div>
+		</nav>
 
 <!--------------------- Home section ----------------------------------->
 
