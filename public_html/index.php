de25112e566483c3cf41c32f6ca131fe39cfa511
Edith Thakkar <egarcia262@cnm.edu>
2018-05-26 13:22:16 -0600

    testing of css style

--- public_html/index.php
+++ public_html/index.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<html lang="en">
 	<head>
 		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Edith Thakkar PWP</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<!--BOOTSTRAP CSS-->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
 		<!--jQuery first, then popper.js, then Bootstrap JS -->
-
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jQuery-slimScroll/1.3.8/jquery.slimscroll.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"</script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"</script>
 
 		<!--Fonts-->
 		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
@@ -23,51 +22,45 @@
 		<!-- JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
 	</head>
-<body>
-	<div>
-		<!--Navigation bar-->
-			<nav class="navbar">
-			<div class style="text-align: center" data-spy="scroll" data-target='.navbar' data-offset='50'>
-				<nav class="navbar navbar-expand-sm bg dark navbar-dark sticky-top"></nav>
-				<ul class="navbar-nav">
-					<li class="nav-item"><a href="#HOME" class="nav-link active">HOME</a></li>
-					<li class="nav-item"><a href="#ABOUT US" class="nav-link active">ABOUT US</a></li>
-					<li class="nav-item"><a href="#TRAVEL" class="nav-link active">TRAVEL</a></li>
-					<li class="nav-item"><a href="#CONTACT US" class="nav-link active">CONTACT US</a></li>
-				</ul>
-			</div>
-					</div>
-			</>
+	<body>
 
-		<!--Logo-->
-			<div class="logo"><a href="#"><img src="images/LogoMakr_7iiiAg.png"></a>
-			</div>
-		<!--Home section-->
+<!--------------------- Navigation bar ----------------------------------->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+	<a class="nav-brand" href="#"><img src="images/LogoMakr_7iiiAg%20copy2.png"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse"
+	data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+<div class="collapse navbar-collapse" id="navbarResponsive">
+	<ul class="navbar-nav ml-auto">
+		<li class="nav-item active">
+			<a class="nav-link" href="#">Home</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">About Us</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Travel</a>
+		</li>
+	<li class="nav-item">
+		<a class="nav-link" href="#">Contact Us</a>
+	</li>
+</div>
+</div>
+</nav>
+
+<!--------------------- Home section ----------------------------------->
 
 	<div id="HOME" class="container-fluid" style="padding-top:70px;padding-bottom:70px"></div>
 		<h1>HOME</h1>
 		<p>Welcome to Travel track. Travel track is an informational travel site where you can find ideas for your next out of country vacation. Where your able to not only find suggestions, but are able to get connected with tour guides, and host families</p>
-
-
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
 
 			<!--About Us section-->
 	<div id="ABOUT US" class="container-fluid" style="padding-top:70px;padding-bottom:70px">
 		<h1>ABOUT US</h1>
 		<p>Travel Track was created with an idea of coming up with ways for travelers to get recommendations to fuel their travel desires. Not only are they recommended places to travel to but are also given resources to help them manage their travels more easily</p>
 	</div>
-
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
 
 			<!--Travel section-->
 	<div id="TRAVEL" class="container-fluid" style="padding-top:70px;padding-bottom:70px">
@@ -76,21 +69,40 @@
 		<p>Chupa chups tootsie roll marzipan. Cupcake chupa chups marshmallow. Tootsie roll tootsie roll dragée. Dessert ice cream donut cotton candy. Cupcake brownie marshmallow. Danish bonbon lemon drops. Dessert tart macaroon sweet roll chocolate bonbon icing pie. Pudding icing chocolate cake chocolate cake bear claw tootsie roll. Apple pie sesame snaps chupa chups jelly beans. Brownie macaroon jelly-o caramels brownie marzipan gingerbread ice cream. Danish tootsie roll chocolate cake jujubes marshmallow muffin. Bear claw muffin cupcake toffee marshmallow tart bonbon gummies biscuit.</p>
 	</div>
 
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-
 			<!--Contact Us section-->
 
-	<body>
-		<header>
-			<h1>Contact Us</h1>
-		</header>
-		<div class="contact">
-		<form id="contact" method="post" action="">
-			<h2>Contact Form</h2>
-		<label>Name :</label><br>
-		<input type="text" placeholder=""Enter u R name"
+				<section>
+					<div class="container">
+						<div class="form-wrapper">
+							<h2>Contact Us Form</h2>
+							<form>
+
+								<div>
+									<label for="name">Name</label>
+									<input type="text" name="name" id="name"/>
+								</div>
+
+								<div>
+									<label for="emailEmail">Email</label>
+									<input type="email" name="email" id="email"/>
+								</div>
+
+								<div>
+									<label for="subject">Subject</label>
+									<input type="text" name="subject" id="subject"/>
+								</div>
+
+								<div>
+									<label for="message">Message</label>
+									<textarea name="message" id="message" cols="30" rows="10" maxlength="1000" placeholder="1000 characters max"></textarea>
+								</div>
+
+								<button type="submit">Send!</button>
+								<button type="reset">Reset</button>
+							</form>
+						</div>
+					</div>
+				</section>
+	</section>
+	</body>
+</html>
