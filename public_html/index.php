fbe452602a5eb9afef14d1c60ac808f0f1c489a2
Edith Thakkar <egarcia262@cnm.edu>
2018-05-27 12:15:42 -0600

    text alignment for div container

--- public_html/index.php
+++ public_html/index.php
@@ -13,11 +13,11 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
-
 		<!-- jQuery Form, Additional Methods, Validate -->
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.81/baguetteBox.min.js"></script>
 
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
@@ -36,6 +36,7 @@
 
 		<!-- Linked CSS stylesheet-->
 		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.81/baguetteBox.min.css">
 
 		<!-- JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
@@ -70,36 +71,40 @@
 		</nav>
 
 		<!--Carousel-->
-		<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+		<div id="carouselExampleFade" class="carousel slide" data-ride="carousel">
 			<div class="carousel-inner">
 				<div class="carousel-item active">
-					<img class="d-block w-100" src="images/ocean.jpg" alt="First slide">
+					<img class="d-block w-100" src="images/menton-old-town-harbour-entrance-lighthouse-161098.jpg" alt="First slide">
+					<div class="carousel-caption d-none d-md-block">
+					<h2>Welcome</h2>
+					<p>Unleash your inner travel spirit</p
+					</div>
 				</div>
+				</div>
+
 				<div class="carousel-item">
 					<img class="d-block w-100" src="images/city.jpg" alt="Second slide">
 				</div>
+
 				<div class="carousel-item">
 					<img class="d-block w-100" src="images/trees.jpg" alt="Third slide">
 				</div>
 			</div>
-			<a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
+
+		<div class="carousel-item">
+			<img class="d-block w-100" src="images/resort.jpg" alt="Second slide">
 		</div>
 
+		<div class="carousel-item">
+			<img class="d-block w-100" src="images/lake.jpg" alt="Second slide">
+		</div>
 
-
-
-		<!--Home section-->
 				<!--About Us section-->
 	<div id="ABOUTUS" class="container-fluid" style="padding-top:70px;padding-bottom:70px">
 		<h1>ABOUT US</h1>
 		<p>Travel Track was created with an idea of coming up with ways for travelers to get recommendations to fuel their travel desires. Not only are they recommended places to travel to but are also given resources to help them manage their travels more easily</p>
+
+		<p>Travel track founders Edith Thakkar, and Vishal Thakkar created travel track as a way to help travelers find resources for traveling abroad such as accomedation suggestions, travel ideas, and so forth.</p>
 	</div>
 
 			<!--Travel section-->
@@ -108,6 +113,60 @@
 		<p>Where would you like to travel to? To Asia, South America, Africa, or maybe Europe?</p>
 		<p>Chupa chups tootsie roll marzipan. Cupcake chupa chups marshmallow. Tootsie roll tootsie roll dragée. Dessert ice cream donut cotton candy. Cupcake brownie marshmallow. Danish bonbon lemon drops. Dessert tart macaroon sweet roll chocolate bonbon icing pie. Pudding icing chocolate cake chocolate cake bear claw tootsie roll. Apple pie sesame snaps chupa chups jelly beans. Brownie macaroon jelly-o caramels brownie marzipan gingerbread ice cream. Danish tootsie roll chocolate cake jujubes marshmallow muffin. Bear claw muffin cupcake toffee marshmallow tart bonbon gummies biscuit.</p>
 	</div>
+
+<section>
+	<div class="container">
+		<div class="container gallery-container">
+			<h1 class="text-center">Places to Visit</h1>
+			<p class="page-description text-center">North America-South America-Asia-Europe</p>
+			<div class="tz-gallery">
+				<div class="row mb-3">
+					<div class="col-md-4">
+						<h3>Europe</h3>
+						<div class="card">
+							<a class="lightbox" href="images/europe-travel-landmarks-vector-illustration-74315910.jpg">
+								<img src="images/europe-travel-landmarks-vector-illustration-74315910.jpg" alt="Europe" class="card-img-top">
+							</a>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<h3>North America</h3>
+						<div class="card">
+							<a class="lightbox" href="images/vector-north-america-travel-flat-format-eps-colors-49842440.jpg">
+								<img src="images/vector-north-america-travel-flat-format-eps-colors-49842440.jpg" alt="North America" class="card-img-top">
+							</a>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<h3>South America</h3>
+						<div class="card">
+							<a class="lightbox" href="images/vector-south-america-travel-latin-set-cultural-flat-color-eps-49988656.jpg">
+								<img src="images/vector-south-america-travel-latin-set-cultural-flat-color-eps-49988656.jpg" alt="South Amercica" class="card-img-top">
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-4">
+						<h3>Asia</h3>
+						<div class="card">
+							<a class="lightbox" href="images/935380352.jpg">
+								<img src="images/935380352.jpg" alt="Asia" class="card-img-top">
+							</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+</section>
+
 
 			<!--Contact Us section-->
 				<section>
@@ -122,17 +181,17 @@
 
 										<div>
 											<label for="name">Name</label>
-											<input type="text" name="name" id="name"/>
+											<input type="text" name="name" id="name" placeholder="Name">
 										</div>
 
 										<div>
 											<label for="email">Email</label>
-											<input type="email" name="email" id="email"/>
+											<input type="text" name="email" id="name" placeholder="Email">
 										</div>
 
 										<div>
 											<label for="subject">Subject</label>
-											<input type="text" name="subject" id="subject"/>
+											<input type="text" name="subject" id="name" placeholder="Subject">
 										</div>
 
 										<div>
